5ebd58f49054d5663c798a31b85933f1fea9e552
Jeffrey Smith <jsmith6690@Jeffreys-MacBook-Pro.local>
2017-10-08 18:12:01 -0400

    dev

--- src/railroad/core/actions/projects.ts
+++ src/railroad/core/actions/projects.ts
@@ -6,6 +6,11 @@ export const GET_SUCCESS: string = 'GET_SUCCESS';
 
 export class Get implements Action {
     public readonly type: string = GET_PROJECTS;
+    public payload: Project[];
+
+    constructor(projects?: Project[]) {
+        this.payload = projects;
+    }
 }
 
 export class GetSuccess implements Action {
